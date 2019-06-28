43c548794d42d8f1f557155afedcb825350e864e
Abhishek D S <abhishekds94@gmail.com>
2019-06-28 19:37:22 +0530

    Tax value addition in Goods - Public above 40000

--- app/src/main/java/com/avidprogrammers/insurancepremiumcalculator/ppdisplay_com_goods_public_40000above.java
+++ app/src/main/java/com/avidprogrammers/insurancepremiumcalculator/ppdisplay_com_goods_public_40000above.java
@@ -53,7 +53,8 @@ public class ppdisplay_com_goods_public_40000above extends AppCompatActivity imp
     CheckingStatus checkingStatus;
 
     private AdView mAdView;
-
+    private double tax_18;
+    private double tax_12;
     long diffInDays;
     double dop_value;
     float rounded_dop_value;
@@ -81,6 +82,8 @@ public class ppdisplay_com_goods_public_40000above extends AppCompatActivity imp
     TextView ppdisplay_com_goods_public_40000above_od_value;
     TextView ppdisplay_com_goods_public_40000above_b_value;
     TextView ppdisplay_com_goods_public_40000above_ab_value;
+    TextView ppdisplay_com_goods_public_40000above_tax18_value;
+    TextView ppdisplay_com_goods_public_40000above_tax12_value;
     TextView ppdisplay_com_goods_public_40000above_total_value;
 
     @Override
@@ -121,6 +124,9 @@ public class ppdisplay_com_goods_public_40000above extends AppCompatActivity imp
          ppdisplay_com_goods_public_40000above_od_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_40000above_od_value);
          ppdisplay_com_goods_public_40000above_b_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_40000above_b_value);
          ppdisplay_com_goods_public_40000above_ab_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_40000above_ab_value);
+         ppdisplay_com_goods_public_40000above_tax12_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_40000above_tax12_value);
+         ppdisplay_com_goods_public_40000above_tax18_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_40000above_tax18_value);
+         ppdisplay_com_goods_public_40000above_total_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_40000_total_value);
          ppdisplay_com_goods_public_40000above_total_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_40000above_total_value);
         ppdisplay_com_goods_public_40000above_total_idv = ((TextView) findViewById(R.id.ppdisplay_com_goods_public_40000above_IDV_value));
         ppdisplay_com_goods_public_40000above_total_idv.setText(b.getCharSequence("pp_com_goods_public_40000above_idv_value"));
@@ -268,10 +274,21 @@ public class ppdisplay_com_goods_public_40000above extends AppCompatActivity imp
         double total_b = (double) ((((Integer.valueOf("41561").intValue() + Integer.valueOf(b.getCharSequence("pp_com_goods_public_40000above_paod_value").toString()).intValue()) + Integer.valueOf("50").intValue()) + (Integer.valueOf(b.getCharSequence("pp_com_goods_public_40000above_coolie").toString()).intValue() * 50)) + (Integer.valueOf(b.getCharSequence("pp_com_goods_public_40000above_nfpp").toString()).intValue() * 75));
         ppdisplay_com_goods_public_40000above_b_value.setText(String.valueOf((int) total_b));
         double round_value = total_b + ((double) this.rounded_ncb_value);
-        int rounded_total_premium_ab_int = (int) new Float((float) Math.round(round_value + ((((double) Integer.valueOf("18").intValue()) * round_value) / 100.0d))).floatValue();
+
+        //To calculate 18% tax
+        tax_18 = ((((round_value - 41561) * (double) Integer.valueOf("18").intValue())) / 100.0d);
+        int tax_18_final = (int)Math.round(tax_18);
+        ppdisplay_com_goods_public_40000above_tax18_value.setText(String.valueOf(tax_18_final));
+
+        //To calculate 12% tax
+        tax_12 = 41561*0.12;
+        int tax_12_final = (int)Math.round(tax_12);
+        ppdisplay_com_goods_public_40000above_tax12_value.setText(String.valueOf(tax_12_final));
+
+        //int rounded_total_premium_ab_int = (int) new Float((float) Math.round(round_value + ((((double) Integer.valueOf("18").intValue()) * round_value) / 100.0d))).floatValue();
+        int rounded_total_premium_ab_int = (int) new Float((float) Math.round(round_value + tax_18_final + tax_12_final)).floatValue();
         ppdisplay_com_goods_public_40000above_ab_value.setText(String.valueOf((int) round_value));
         ppdisplay_com_goods_public_40000above_total_value.setText(String.valueOf(rounded_total_premium_ab_int));
-
     }
 
     View.OnClickListener listener_ppdisplay_com_goods_public_40000above_home = new View.OnClickListener() {
