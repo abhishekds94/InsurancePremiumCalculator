d1de1c899563a5d0c78472fedd88f2d68659e64a
Abhishek D S <abhishekds94@gmail.com>
2019-06-28 18:40:44 +0530

    Tax value addition in Goods - Public upto 7500

--- app/src/main/java/com/avidprogrammers/insurancepremiumcalculator/ppdisplay_com_goods_public_7500.java
+++ app/src/main/java/com/avidprogrammers/insurancepremiumcalculator/ppdisplay_com_goods_public_7500.java
@@ -54,7 +54,8 @@ public class ppdisplay_com_goods_public_7500 extends AppCompatActivity implement
     CheckingStatus checkingStatus;
 
     private AdView mAdView;
-
+    private double tax_18;
+    private double tax_12;
     long diffInDays;
     double dop_value;
     float rounded_dop_value;
@@ -82,6 +83,8 @@ public class ppdisplay_com_goods_public_7500 extends AppCompatActivity implement
     TextView ppdisplay_com_goods_public_7500_od_value;
     TextView ppdisplay_com_goods_public_7500_b_value;
     TextView ppdisplay_com_goods_public_7500_ab_value;
+    TextView ppdisplay_com_goods_public_7500_tax18_value;
+    TextView ppdisplay_com_goods_public_7500_tax12_value;
     TextView ppdisplay_com_goods_public_7500_total_value;
 
     @Override
@@ -121,6 +124,8 @@ public class ppdisplay_com_goods_public_7500 extends AppCompatActivity implement
          ppdisplay_com_goods_public_7500_od_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_7500_od_value);
          ppdisplay_com_goods_public_7500_b_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_7500_b_value);
          ppdisplay_com_goods_public_7500_ab_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_7500_ab_value);
+         ppdisplay_com_goods_public_7500_tax18_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_7500_tax18_value);
+         ppdisplay_com_goods_public_7500_tax12_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_7500_tax12_value);
          pp_com_goods_public_7500_paod_value = findViewById(R.id.ppdisplay_com_goods_public_7500_pa_value);
          ppdisplay_com_goods_public_7500_total_value = (TextView) findViewById(R.id.ppdisplay_com_goods_public_7500_total_value);
         ppdisplay_com_goods_public_7500_total_idv = ((TextView) findViewById(R.id.ppdisplay_com_goods_public_7500_IDV_value));
@@ -280,10 +285,21 @@ public class ppdisplay_com_goods_public_7500 extends AppCompatActivity implement
         double total_b = (double) ((((Integer.valueOf("15746").intValue() + Integer.valueOf(b.getCharSequence("pp_com_goods_public_7500_paod_value").toString()).intValue()) + Integer.valueOf("50").intValue()) + (Integer.valueOf(b.getCharSequence("pp_com_goods_public_7500_coolie").toString()).intValue() * 50)) + (Integer.valueOf(b.getCharSequence("pp_com_goods_public_7500_nfpp").toString()).intValue() * 75));
         ppdisplay_com_goods_public_7500_b_value.setText(String.valueOf((int) total_b));
         double round_value = total_b + ((double) this.rounded_ncb_value);
-        int rounded_total_premium_ab_int = (int) new Float((float) Math.round(round_value + ((((double) Integer.valueOf("18").intValue()) * round_value) / 100.0d))).floatValue();
+
+        //To calculate 18% tax
+        tax_18 = ((((round_value - 15746) * (double) Integer.valueOf("18").intValue())) / 100.0d);
+        int tax_18_final = (int)Math.round(tax_18);
+        ppdisplay_com_goods_public_7500_tax18_value.setText(String.valueOf(tax_18_final));
+
+        //To calculate 12% tax
+        tax_12 = 15746*0.12;
+        int tax_12_final = (int)Math.round(tax_12);
+        ppdisplay_com_goods_public_7500_tax12_value.setText(String.valueOf(tax_12_final));
+
+        //int rounded_total_premium_ab_int = (int) new Float((float) Math.round(round_value + ((((double) Integer.valueOf("18").intValue()) * round_value) / 100.0d))).floatValue();
+        int rounded_total_premium_ab_int = (int) new Float((float) Math.round(round_value + tax_18_final + tax_12_final)).floatValue();
         ppdisplay_com_goods_public_7500_ab_value.setText(String.valueOf((int) round_value));
         ppdisplay_com_goods_public_7500_total_value.setText(String.valueOf(rounded_total_premium_ab_int));
-
     }
 
     View.OnClickListener listener_ppdisplay_com_goods_public_7500_home = new View.OnClickListener() {
